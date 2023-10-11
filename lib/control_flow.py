f26cc1d0213bf445ce075c3cfbebc0e833190717
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-10-11 11:44:46 +0300

    Assignment complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,50 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    return "Access granted" if (username == "admin" or username=="ADMIN") and password == "12345" else "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40 :
+        return "It's brisk out there!"
+    elif 40 <= temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    elif temperature == 75:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+   if num % 3 == 0 and num % 5 == 0:
+       return "FizzBuzz"
+   elif num % 5 == 0:
+       return "Buzz"
+   elif  num % 3 == 0:
+       return "Fizz"
+   else:
+       return num
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0: 
+         return num1 / num2
+        else:
+            print("Denominator cannot be zero!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+
+
+credentials = admin_login("ADMIN", "12345")
+print(credentials)
+
+print(hows_the_weather(50))
+print(fizzbuzz(15))
+print(calculator("+",1,1))
